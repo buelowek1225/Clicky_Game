b666b91ca2c7c3a8f65914483002ad18040ccf6a
Ellen Buelow <buelowek1225@gmail.com>
2019-06-21 22:16:05 -0500

    Updates

---
+++ precache-manifest.24294ba1914cd380004cb46a4c5db28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09887637118fb1d9deb0b101140cdad",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "4026a434af9b1acfbebe",
+    "url": "/Clicky_Game/static/css/main.8b3342ef.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky_Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4026a434af9b1acfbebe",
+    "url": "/Clicky_Game/static/js/main.d7323b2b.chunk.js"
+  },
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  }
+]);
